83adab0d62785e114256109c59047919e4d70cea
vanek1112 <chernov.ivan1112@gmail.com>
2025-05-14 03:10:39 +0500

    Ready to deploy

--- app/database.py
+++ app/database.py
@@ -1,21 +1,18 @@
+import os
 from sqlalchemy import Column, String, UUID, Enum, Integer, Float, ForeignKey, DateTime, NullPool
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel, Field, validator
 import psycopg2
-import ssl
 
 def create_psycopg2_connection():
-    return psycopg2.connect("""
-    host=rc1a-202opm4h7wa25idi.mdb.yandexcloud.net,rc1b-u55xsfzl9r2181yo.mdb.yandexcloud.net
-    port=6432
-    sslmode=verify-full
-    dbname=tochka
-    user=chernov
-    password=chernov_tochka2025
-    target_session_attrs=read-write
-""")
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"))
 
 engine = create_engine(
     "postgresql+psycopg2://",
