1e3f30f2d4d75aa3bfbb11cb347a050bc1b88e6c
vanek1112 <chernov.ivan1112@gmail.com>
2025-06-07 22:17:42 +0500

    HotFix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
+import logging
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.database import storage
+from app.database import storage, Storage
 from app.routes import public, user, admin
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
-app = FastAPI(title="Toy exchange", version="0.1.0")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info(f"Admin API Key: {storage.admin_api_key}")
+    print(f"Admin API Key via print: {storage.admin_api_key}")
+    yield
+    logger.info("Bye.")
+
+app = FastAPI(lifespan=lifespan, title="Toy exchange", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
